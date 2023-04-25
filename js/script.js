e7acce1cd0a7d6484a2c87fc8748c7d50035a6bb
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-04-25 20:01:26 +0300

    фввув

--- js/script.js
+++ js/script.js
@@ -99,18 +99,17 @@ function burgerMenu(selector) {
   
   
   
-  
-  
-  $('.scrollto a').on('click', function() {
-  
-    let href = $(this).attr('href');
-  
-    $('html, body').animate({
-        scrollTop: $(href).offset().top
-    }, {
-        duration: 370,   // по умолчанию «400» 
-        easing: "linear" // по умолчанию «swing» 
-    });
-  
-    return false;
-  });
+  const anchors = document.querySelectorAll('a[href*="#"]')
+
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault()
+      
+      const blockID = anchor.getAttribute('href').substr(1)
+      
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
+    })
+  }
